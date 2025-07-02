0fc104a72f0e1b6802d907c5a05b0325089ae990
annavighi <vighianna@gmail.com>
2025-07-02 13:42:05 -0300

    desafio concluído e entregue na plataforma da DIO

---
+++ condicionais/desafio-condicionais/src/Mjolnir.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Mjolnir {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int C = leitor.nextInt();
+        String nome;
+        int N;
+
+        for (int i = 0; i < C; i++) {
+            nome = leitor.next();
+            N = leitor.nextInt();
+            leitor.close();
+            if (nome.equals("Thor")) {
+                System.out.println("Y"); 
+            }else {
+                System.out.println("N");
+            }
+        }
+    }
+
+}
